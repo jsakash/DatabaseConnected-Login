5ab7d3f8b3aec0ed7e86b1a04c8ab476d2f19178
Akash JS <akashjs710@gmail.com>
2022-08-04 19:05:06 +0530

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,8 @@
+# User And Admin 
+
+This is a sample project built for User and Admin login
+
+- User Authentication is done
+- JWT token based authentication is used
+- Admin can edit, block, unblock, create Users
+- Database used is Postgresql (PSQL) 
---
+++ routes/userRouter.go
@@ -0,0 +1,19 @@
+package routes
+
+import (
+	controller "project_login/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.LoadHTMLGlob("templates/*.html")
+	incomingRoutes.GET("/wadmin", controller.Wadmin)
+	incomingRoutes.GET("/home", controller.Home)
+	incomingRoutes.GET("/delete/:name", controller.DeleteUser)
+	incomingRoutes.POST("/update/:name", controller.UpdateUser)
+	incomingRoutes.POST("/create", controller.PostCreateUser)
+	incomingRoutes.POST("/block/:name", controller.BlockUser)
+	incomingRoutes.POST("/unblock/:name", controller.UnblockUser)
+
+}
